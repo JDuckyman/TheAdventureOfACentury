fb2f8950b66e86f7defce873177fa9e5b249577d
Paul Dilyard <paul.dilyard@gmail.com>
2013-10-07 14:47:48 -0400

    Improved inventory function

--- inventory.py
+++ inventory.py
@@ -1,2 +1,7 @@
-def take(itemName, itemsAvailable, currentInventory):
-	currentInventory[itemName] = itemsAvailable[itemName]
+def addItems(location, items, currentInventory, newItems):
+    items[newItems] = newItems
+    if newItems in currentInventory:
+        items.pop(newItems)
+
+def take(itemName, itemsAvailable, inventory):
+    inventory[itemName] = itemsAvailable[itemName]
--- main.py
+++ main.py
@@ -4,48 +4,38 @@ import inventory
 
 location = [4, 4]
 play = 1
-available = 0
-apple = False
-haveApple = False
-orange = False
-haveOrange = False
+items = {}
+currentInventory = {}
 
 while play == 1:
 
-	#Defining object locations
-	if location == [4,4] and haveApple == False:
-		apple = True
-	else:
-		apple = False
-	if location == [4,5] and haveOrange == False:
-		orange = True
-	else:
-		orange = False
+    #Defining object locations
+    items.clear()
+    if location == [4,4]:
+        inventory.addItems(location, items, currentInventory, 'apple')
+    elif location == [4,5]:
+        inventory.addItems(location, items, currentInventory, 'orange')
 
+    print(' ')
+    action = input('Type what you would like to do: ')
+    print(' ')
+    commands = action.split()
 
-	print(' ')
-	action = input('Type what you would like to do: ')
-	print(' ')
-	commands = action.split()
+    # Movement
+    if commands[0] == 'move':
+        location = movement.move(commands[1], location[0], location[1])
 
-	# Movement
-	if commands[0] == 'move':
-		location = movement.move(commands[1], location[0], location[1])
+    # Help
+    elif commands[0] == 'help':
+        help.listCommands()
 
-	# Help
-	elif commands[0] == 'help':
-		help.listCommands()
+    #Inventory
+    elif commands[0] == 'take':
+        inventory.take(commands[1], items, currentInventory)
 
-	#Inventory
-	elif commands[0] == 'take':
-		if commands[1] == 'apple' and apple == True:
-			haveApple = True
-		if commands[1] == 'orange' and orange == True:
-			haveOrange = True
+    elif commands[0] == 'inv':
+        for value in currentInventory.values():
+            print(value)
 
-
-	elif commands[0] == 'inv':
-			if haveApple == True:
-				print('Apple')
-			if haveOrange == True:
-				print('Orange')
+    elif commands[0] == 'search':
+        print(items)
